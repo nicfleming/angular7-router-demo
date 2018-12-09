cb73996e13d3e865657fadf968343dc11072a0c8
Nick Fleming <nicfleming@deloitte.com>
2018-12-09 13:12:09 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { BackendService } from './backend.service';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { AppRoutingModule} from './app-routing.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InMemoryWebApiModule.forRoot(BackendService),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
